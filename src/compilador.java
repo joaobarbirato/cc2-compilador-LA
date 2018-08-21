6382272445df9b2ee0bbf1e9c0179ca9274b9a49
cassianomaia <cassmala@gmail.com>
2018-08-21 10:29:38 -0300

    Criação da classe compilador

---
+++ src/compilador.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.RecognitionException;
+
+
+
+public class Compilador{
+
+
+
+    public static void main(String[] args) throws IOException, RecognitionException {
+        ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(args[0])); //Essa linha garante que estamos pegando os casos de teste
+    }
+
+}
